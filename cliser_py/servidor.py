e279fa40dc01e66e13364c8514aac4d9bd1a4e2f
Luiz Paulo de Lima Araújo <pluiz0674@gmail.com>
2023-10-21 16:27:23 -0300

    funcionalidade/servidor-tcp (comunicacao) modifica_servidor_e_cliente
    Implementa socket no servidor capaz de responder a uma requisicao
    sem paralelismo. cliente simples envia dados puros ao servidor
    via telnet.

--- cliser_py/servidor.py
+++ cliser_py/servidor.py
@@ -1,14 +1,60 @@
 import socket       # comunicação
 import threading    # paralelismo
 import os           # funcionalidades do sistema (UNIX/LINUX)
+import json
 
-class Servidor
+# SERVIDOR TCP C/python
 
-    def __init__( self ):
-        self.id = os.getPid()
+"""
+socket()  Instância um "endpoint"
+bind()    Acopla uma porta ao socket instânciado
+listen()  Escuta aguardando por conexões
+ accept()  Aceitação de conexão
+  connect() Estabelecimento da comunicação
+   send()    Mandar dados
+   recv()    Receber dados
+  shutdown()   Encerrar conexão?
+close()   Exterminar socket
+"""
 
-    def escutar( self ):
-        pass
+# servidor que faz divisões
+# cria socket, atende requisição, mata socket
+class Servidor:
 
-app = Servidor()
+    def __init__( self, porta ):
+
+        self.HOST = socket.gethostbyname( socket.gethostname() )
+        self.PORTA = porta
+
+        self.id = os.getpid()
+
+        self.s = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+        self.s.bind( ( self.HOST, self.PORTA ) )
+        self.s.listen()
+
+    # processa divisão para {"a":10,"b":2} requisitado PROTECTED
+    def divisao( self, byteStr ):
+
+        dic = {}
+        encode = 'utf-8'
+
+        dic = json.loads( byteStr.decode( encode ) )
+        resultado = ( dic['a'] / dic['b'] )
+        resp = '{\"r\":' + str( resultado ) + '}'
+        return bytes( resp, encode )
+            
+
+    # aceita conexão, responde requisição e extermina o socket
+    def aceitarConexao( self ):
+            while True:
+                conn, addr = self.s.accept()
+                dados = conn.recv( 32 )
+                print(dados)
+                if dados:
+                    conn.send( self.divisao( dados ) )
+                    self.s.close()
+                    break
+
+app = Servidor( 8080 )
+app.aceitarConexao()
 
